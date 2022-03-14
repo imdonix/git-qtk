e484906372b204bbb94f37f1d5f787ebffadce92
imdonix <tamas.donix@gmail.com>
2022-03-14 19:23:34 +0100

    updated app exports

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 const { Query } = require('./core/query')
+const Plugin = require('./core/plugin')
+const Model = require('./core/model')
 
-module.exports = Query;
+module.exports = { Query, Plugin, Model };
--- pg.js
+++ pg.js
@@ -2,3 +2,6 @@ const Git = require('nodegit')
 const fs = require('fs')
 
 console.log(fs.readdirSync(`${__dirname}/plugins`))
+
+
+let r = new Git.Repository()
