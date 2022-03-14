ce3c2a5ed5132aca0427f3847278837f996b8aa4
imdonix <tamas.donix@gmail.com>
2022-03-14 18:51:58 +0100

    added commit parsing to git plugin

--- plugins/git.js
+++ plugins/git.js
@@ -1,6 +1,7 @@
 const Plugin = require('../core/plugin')
 
 const Author = require('../models/author')
+const Commit = require('../models/commit')
 
 class Git extends Plugin
 {
@@ -9,16 +10,18 @@ class Git extends Plugin
     {
         super()
         this.auth = new Author()
+        this.commit = new Commit()
     }
 
     models()
     {
-        return [ this.auth ]
+        return [ this.auth, this.commit ]
     }
 
     parse(db, commit)
     {
         db.add(this.auth, this.auth.parse(commit.author()))
+        db.add(this.commit, this.commit.parse(commit))
     }
 
 }
