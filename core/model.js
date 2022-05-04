2220fd573e7a463fc7421ae81518243d303abed5
Magyar Tamás <tamas.donix@gmail.com>
2022-05-04 17:34:23 +0200

    finished user documentaiton

--- core/model.js
+++ core/model.js
@@ -3,12 +3,16 @@ const { abs } = require('./utils')
 
 class Model
 {
+    //name of the model
     name() { abs() }
 
+    //the model definition as a name-type object 
     model() { abs() }
 
+    //the unique key of the model
     key(){ abs() }
 
+    //the processor which creates the actual database object
     parse(input) { abs() }
 
     has(field)
--- core/plugin.js
+++ core/plugin.js
@@ -2,18 +2,26 @@ const { abs } = require('./utils')
 
 class Plugin
 {
+    //the name of the plugin as a string
     name() { abs() }
 
+    //a list of the functions which are injected into the statements
     functions() { abs() }
 
+    //a list of the reductors which are injected into the statements
     reductors() { abs() }
 
+    //a list of the instantiated models
     models() { abs() }
 
+
+    //run at the start of the parsing
     async init() { abs() }
 
+    //run for each commit
     async parse(db, commit) { abs() }
 
+    //run at the end of the parsing
     async post() { abs() }
 }
 
